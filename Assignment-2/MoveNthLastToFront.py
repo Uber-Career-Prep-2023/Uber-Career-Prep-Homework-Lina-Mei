c87392254d139da27391dd87df8316c4c57154a4
Lina Mei <lmei25@illinois.edu>
2023-05-08 05:14:28 -0500

    Finished MoveNthLastToFront

--- Assignment-2/MoveNthLastToFront.py
+++ Assignment-2/MoveNthLastToFront.py
@@ -2,9 +2,14 @@
 Given a singly linked list, move the nth from the last element to the front of the list.
 
 
-Time Complexity:
-Space Complexity:
+Time Complexity: O(N)
+Space Complexity: O(1)
 
+25 minutes taken total
+
+Explanation:
+    The time complexity is O(n) because worst case scenario, we have to traverse to the end of the list to get the last element of the list.
+    The space complexity is O(1) because we are only using variables which runs at constant time.
 """
 
 from SinglyLinkedList import *
